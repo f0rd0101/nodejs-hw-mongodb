1ac3557fca61f9bc1c4fd860a91b97e2b379be17
Nick <melancholic8078@gmail.com>
2025-06-19 17:19:55 +0300

    done

---
+++ src/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import { getEnvVar } from './utils/getEnvVar.js';
+import cors from 'cors';
+import pino from 'pino-http';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = async () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.send('Helo World!');
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (contact === null) {
+      return res.status(404).send({
+        status: 404,
+        message: 'Contact not found',
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    if (err.code === 'EADDRINUSE') {
+      console.error(`❌ Port ${PORT} is already in use.`);
+    } else {
+      console.error('❌ Failed to start server:', err);
+    }
+    process.exit(1);
+  }
+};
